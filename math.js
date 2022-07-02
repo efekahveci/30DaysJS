f748f92d418db0ac09e114fec1db53eb0178efcc
efekahveci <efekhvci@hotmail.com>
2022-07-02 17:45:30 +0300

    2 day complate

---
+++ math.js
@@ -0,0 +1,6 @@
+console.log(Math.PI)
+
+let round = Math.round(3.13)
+let floor = Math.floor(3.222)
+
+console.log("round 3.13 -> ",round,"floor 3.2222 ->",floor)
---
+++ specialCharacters.js
@@ -0,0 +1,6 @@
+console.log("newline -> \nnewline")
+console.log("tab -> \ttab")
+console.log("backslash -> \\backslash")
+console.log("quote -> \'quote")
+console.log("doublequote -> \"doublequote")
+
